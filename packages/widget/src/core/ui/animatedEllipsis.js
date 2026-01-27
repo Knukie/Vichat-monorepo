c7a69542c5e3ed7ac84219fead9e5e3956b21580
Knukie <ianvdvalk@gmail.com>
2026-01-27 14:16:34 +0100

    Fix sources ellipsis animation

--- packages/widget/src/core/ui/animatedEllipsis.js
+++ packages/widget/src/core/ui/animatedEllipsis.js
@@ -1,13 +1,6 @@
-const DEFAULT_INTERVAL = 450;
-const FRAMES = ['.', '..', '...'];
-
-export function createAnimatedEllipsis(node, { interval = DEFAULT_INTERVAL } = {}) {
+export function createAnimatedEllipsis(node) {
   if (!node) return () => {};
-  let frame = 0;
-  node.textContent = FRAMES[frame];
-  const timer = window.setInterval(() => {
-    frame = (frame + 1) % FRAMES.length;
-    node.textContent = FRAMES[frame];
-  }, interval);
-  return () => window.clearInterval(timer);
+  node.textContent = '';
+  node.classList.add('valki-ellipsis');
+  return () => node.classList.remove('valki-ellipsis');
 }
